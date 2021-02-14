b5ca45e8abf6df2f2660b88416f15c7bfca777a8
fuchimo <f.masaki.1988@gmail.com>
2021-02-14 22:51:43 +0900

    set strong_parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configuer_permitted_paremeters, if: :devise_controller?
 
   private
+
+  def configuer_permitted_paremeters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birth_date])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
