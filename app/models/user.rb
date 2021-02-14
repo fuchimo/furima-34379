8c2e87bf4833040c52a8833673f2847dbfde1fdd
fuchimo <f.masaki.1988@gmail.com>
2021-02-14 21:46:41 +0900

    set validation user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "名前は全角で入力して下さい"}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "苗字は全角で入力して下さい"}
+    validates :first_name, format: {with: /\A[ァ-ヶ一-]+\z/, message: "名前のフリガナは全角カナで入力して下さい"}
+    validates :last_name, format: {with: /\A[ァ-ヶ一-]+\z/, message: "苗字のフリガナは全角カナで入力して下さい"}
+    validates :birth_date
+  end
 end
