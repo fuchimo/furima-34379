c705312b8abc4c8e6716b8fbe8377970686b98d2
fuchimo <f.masaki.1988@gmail.com>
2021-02-15 14:18:47 +0900

    correct validation user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,23 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name,
-              format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters.', allow_blank: true }
-    validates :last_name,
-              format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters.', allow_blank: true }
-    validates :first_name_reading,
-              format: { with: /\A[ァ-ヶ一-]+\z/, message: 'is invalid. Input full-width katakana characters.', allow_blank: true }
-    validates :last_name_reading,
-              format: { with: /\A[ァ-ヶ一-]+\z/, message: 'is invalid. Input full-width katakana characters.', allow_blank: true }
+    validates :first_name
+    validates :last_name
+    validates :first_name_reading
+    validates :last_name_reading
     validates :birth_date
   end
+
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters.', allow_blank: true } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶ一-]+\z/, message: 'is invalid. Input full-width katakana characters.',
+                         allow_blank: true } do
+    validates :first_name_reading
+    validates :last_name_reading
+  end
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid.', allow_blank: true }
 end
