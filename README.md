f925092ed03f4968846bad767a5bb02ef4183e40
fuchimo <f.masaki.1988@gmail.com>
2021-02-15 21:31:09 +0900

    rename items_column

--- README.md
+++ README.md
@@ -29,7 +29,7 @@
 | condition_id      | integer    | null: false                    |
 | charge_id         | integer    | null: false                    |
 | prefecture_id     | integer    | null: false                    |
-| sending_days_id   | integer    | null: false                    |
+| sending_day_id    | integer    | null: false                    |
 | user              | references | null: false, foreign_key: true |
 
 ### Association
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_092013) do
+ActiveRecord::Schema.define(version: 2021_02_15_122615) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_092013) do
     t.integer "condition_id", null: false
     t.integer "charge_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "sending_days_id", null: false
+    t.integer "sending_day_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
