a4b00a6364331bc391fcffc11604b9acc781c98b
fuchimo <f.masaki.1988@gmail.com>
2021-02-16 08:27:12 +0900

    correct validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,12 +13,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_explain
-    validates :item_price
-    validates :category_id
-    validates :condition_id
-    validates :charge_id
-    validates :prefecture_id
-    validates :sending_day_id
     validates :user
     validates :image
   end
@@ -32,6 +26,5 @@ class Item < ApplicationRecord
   end
 
   validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10000000}
-  validates :item_price, format: { with: /\A[0-9]+\z/, message: 'is invalid.', allow_blank: true }
-
+  
 end
