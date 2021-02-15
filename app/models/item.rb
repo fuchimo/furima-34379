fa4ae4545c0fa24b02914219732ddafbbacc5377
fuchimo <f.masaki.1988@gmail.com>
2021-02-15 17:41:18 +0900

    set validation items_model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
-  
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_explain
+    validates :item_price
+    validates :category_id
+    validates :condition_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :sending_days_id
+    validates :user
+    validates :image
+  end
+
+  with_options numericality: { only_integer: true } do
+    validates :category_id
+    validates :condition_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :sending_days_id
+  end
+
+  validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10000000}
+  validates :item_price, format: { with: /\A[0-9]+\z/, message: 'is invalid.', allow_blank: true }
 
 end
