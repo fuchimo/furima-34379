9462ab8d4d6dbb87e6479076f3a888e4e0c67c57
fuchimo <f.masaki.1988@gmail.com>
2021-02-15 18:14:15 +0900

    set items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,14 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
   end
   
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_explain, :item_price, :category_id, :condition_id, :charge_id, :prefecture_id, :sending_days_id).merge(user_id: params[:user.id])
+  end
+
 end
