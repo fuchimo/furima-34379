dc4549f810c87f02395f3a61473b6c46e1cf5e3c
fuchimo <f.masaki.1988@gmail.com>
2021-02-19 18:59:57 +0900

    add example purchase_address

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -103,5 +103,17 @@ RSpec.describe PurchaseAddress, type: :model do
       @purchase_address.valid?
       expect(@purchase_address.errors.full_messages).to include 'Phone number is invalid'
     end
+
+    it 'user_idが空だと購入ができない' do
+      @purchase_address.user_id = ''
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "User can't be blank"
+    end
+
+    it 'item_idが空だと購入ができない' do
+      @purchase_address.item_id = ''
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "Item can't be blank"
+    end
   end
 end
